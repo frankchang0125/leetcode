2845c4c83a36324d7a0489951da27c6ad32aacb5
Frank Chang <frankchang0125@gmail.com>
2019-04-21 12:23:07 +0800

    617. Merge Two Binary Trees (Easy)

---
+++ 617.merge-two-binary-trees.go
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=617 lang=golang
+ *
+ * [617] Merge Two Binary Trees
+ *
+ * https://leetcode.com/problems/merge-two-binary-trees/description/
+ *
+ * algorithms
+ * Easy (68.85%)
+ * Total Accepted:    162.9K
+ * Total Submissions: 234K
+ * Testcase Example:  '[1,3,2,5]\n[2,1,3,null,4,null,7]'
+ *
+ * Given two binary trees and imagine that when you put one of them to cover
+ * the other, some nodes of the two trees are overlapped while the others are
+ * not.
+ * 
+ * You need to merge them into a new binary tree. The merge rule is that if two
+ * nodes overlap, then sum node values up as the new value of the merged node.
+ * Otherwise, the NOT null node will be used as the node of new tree.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 
+ * Tree 1                     Tree 2                  
+ * ⁠         1                         2                             
+ * ⁠        / \                       / \                            
+ * ⁠       3   2                     1   3                        
+ * ⁠      /                           \   \                      
+ * ⁠     5                             4   7                  
+ * Output: 
+ * Merged tree:
+ * 3
+ * / \
+ * 4   5
+ * / \   \ 
+ * 5   4   7
+ * 
+ * 
+ * 
+ * 
+ * Note: The merging process must start from the root nodes of both trees.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// Reference: http://bit.ly/2UFYMOG
+func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
+    if t1 == nil {
+        return t2
+    }
+
+    if t2 == nil {
+        return t1
+    }
+
+    root := t1
+    root.Val += t2.Val
+    root.Left = mergeTrees(t1.Left, t2.Left)
+    root.Right = mergeTrees(t1.Right, t2.Right)
+
+    return root
+}
