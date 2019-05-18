4452103b622e7bd02919a30518f683f731991d92
Frank Chang <frankchang0125@gmail.com>
2019-05-18 19:55:50 +0800

    94. Binary Tree Inorder Traversal (Medium)

---
+++ 94.binary-tree-inorder-traversal.go
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=94 lang=golang
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (54.50%)
+ * Likes:    1562
+ * Dislikes: 68
+ * Total Accepted:    454.8K
+ * Total Submissions: 805.4K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,3,2]
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+    ans := []int{}
+    inorder(root, &ans)
+    return ans
+}
+
+func inorder(node *TreeNode, ans *[]int) {
+    if node != nil {
+        inorder(node.Left, ans)
+        *ans = append(*ans, node.Val)
+        inorder(node.Right, ans)
+    }
+} 
