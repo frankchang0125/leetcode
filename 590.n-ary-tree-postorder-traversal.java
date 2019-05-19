964ac9aeb734446c53b82b5e979a9791b1a07806
Frank Chang <frankchang0125@gmail.com>
2019-05-19 22:27:14 +0800

    590. N-ary Tree Postorder Traversal (Easy)

---
+++ 590.n-ary-tree-postorder-traversal.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=590 lang=java
+ *
+ * [590] N-ary Tree Postorder Traversal
+ *
+ * https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (67.25%)
+ * Likes:    275
+ * Dislikes: 37
+ * Total Accepted:    37.3K
+ * Total Submissions: 55.5K
+ * Testcase Example:  '{"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}'
+ *
+ * Given an n-ary tree, return the postorder traversal of its nodes' values.
+ * 
+ * For example, given a 3-ary tree:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Return its postorder traversal as: [5,6,3,2,4,1].
+ * 
+ * 
+ * Note:
+ * 
+ * Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        traversal(root, ans);
+        return ans;
+    }
+
+    private void traversal(Node node, List<Integer> ans) {
+        if (node != null) {
+            for (Node child : node.children) {
+                traversal(child, ans);
+            }
+            ans.add(node.val);
+        }
+    }
+}
