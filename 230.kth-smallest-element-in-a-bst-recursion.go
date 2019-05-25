0b33bafd9095cddf6d9e7c5cf0bea3068d823048
Frank Chang <frankchang0125@gmail.com>
2019-05-25 15:25:41 +0800

    230. Kth Smallest Element in a BST (Recursion) (Medium)

---
+++ 230.kth-smallest-element-in-a-bst-recursion.go
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode id=230 lang=golang
+ *
+ * [230] Kth Smallest Element in a BST
+ *
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (51.27%)
+ * Likes:    1112
+ * Dislikes: 39
+ * Total Accepted:    221.1K
+ * Total Submissions: 430.9K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * Given a binary search tree, write a function kthSmallest to find the kth
+ * smallest element in it.
+ * 
+ * Note: 
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠ \
+ * 2
+ * Output: 1
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * Output: 3
+ * 
+ * 
+ * Follow up:
+ * What if the BST is modified (insert/delete operations) often and you need to
+ * find the kth smallest frequently? How would you optimize the kthSmallest
+ * routine?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+    return inorder(root, &k)
+}
+
+func inorder(node *TreeNode, k *int) int {
+    if node != nil {
+        val := inorder(node.Left, k)
+        if *k == 0 {
+            return val
+        }
+
+        *k--
+        if *k == 0 {
+            return node.Val
+        }
+
+        return inorder(node.Right, k)
+    }
+
+    return -1
+}
